f4a98b6ffa6dc80a356668b5d5655c474a2f887a
PrincipieCyupe <cyubahiroprincipie@gmail.com>
2025-03-28 20:36:53 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 // config.js
 const config = {
-  rapidApiKey: '85270c2e8fmshadf2fcbab140ad3p11072bjsn6091dbbd3b34' 
+  rapidApiKey: '85270c2e8fmshadf2fcbab140ad3p11072bjsn6091dbbd3b34',
+  rapidApiHost: 'ai-medical-diagnosis-api-symptoms-to-results.p.rapidapi.com',
+  apiEndpoint: 'https://ai-medical-diagnosis-api-symptoms-to-results.p.rapidapi.com/analyzeSymptomsAndDiagnose?noqueue=1'
 };
